2011c8809676339ec8de978ab4564b775dc2d2c8
ThaDarkLord <daniel.j.sykes92@gmail.com>
2024-03-08 14:21:06 -0600

    added typedef code

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,21 +1,63 @@
 const typeDefs = `
-  type Profile {
-    _id: ID
-    name: String
-    skills: [String]!
+type User {
+    id: ID!
+    userName: String
+    email: String!
+    password: String
+    achievements: [achievement]
+    communities: [Community]
+    comments: [Comment]
   }
 
-  type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
+type Community {
+    id: ID!
+    communityName: String
+    users: [User]
   }
 
-  type Mutation {
-    addProfile(name: String!): Profile
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile(profileId: ID!): Profile
-    removeSkill(profileId: ID!, skill: String!): Profile
+
+type Achievement {
+    id: ID!
+    titleAchievement: String!
+    body: String!
+    createdAt: Date
+    users: [User]
+    comments: [Comment]
+  }
+
+type Comment{
+    id: ID
+    commentBody: String!
+    user: [User]
+    createdAt: Date
+
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query{
+    users: [User] 
+    communities: [Community]
+    achievements: [Achievement]
+    comments: [Comment]
+    user(userName: String!): User
+    achievements(userName: String!): [Achievement]
+    comments(: String!): [Comments]
+
+  }
+
+  type Mutations{
+    addUser(username: String!, email: String!, password: String!): Auth
+    addCommunity(communityName: String!): Community
+    addAchievement(userId: ID!, titleAcheivement: String!): Achievement
+    addComment(achievementId: ID!, commentBody: String!): Achievement
+    removeAchievement(achievementId: ID!): Achievement
+    removeComment(commentId: ID, achievementId: ID): Achievement
   }
 `;
 
 module.exports = typeDefs;
+
