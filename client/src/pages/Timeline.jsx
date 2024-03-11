693e110d2c691894c3aa8dd5e591b0de549edf8e
0Clark30 <austinlaw.clark@outlook.com>
2024-03-11 14:32:44 -0500

    timeline mostly working (in prog)

--- client/src/pages/Timeline.jsx
+++ client/src/pages/Timeline.jsx
@@ -2,46 +2,57 @@ import { Chrono } from "react-chrono";
 import Stack from "react-bootstrap/Stack";
 import { Form } from "react-bootstrap";
 import timelineArray from "../components/Content/timelineContent";
-import { useQuery } from '@apollo/client';
+import { useQuery } from "@apollo/client";
 import { QUERY_ACHIEVEMENTS } from "../utils/queries";
-
+import { useState, useEffect } from "react";
 
 export default function Timeline() {
-
   // eslint-disable-next-line no-unused-vars
-  const {loading, data, error} = useQuery(QUERY_ACHIEVEMENTS)
-  if (error){
-    console.log(error)
-    return
+  const { loading, data, error } = useQuery(QUERY_ACHIEVEMENTS);
+  const [achievementList, setAchievements] = useState([]);
+  if (error) {
+    console.log(error);
+    return;
   }
-  const achievements = data?.achievement || []
+  if (loading) {
+    return "loading";
+  }
+  const achievements = data?.achievements || [];
 
-  
+  // console.log(achievements);
+  // console.log(timelineArray(achievements));
+  const items = timelineArray(achievements);
+  console.log(items);
+  // useEffect(()=>{
+  //   if(achievements){
+  //   setAchievements(achievements)
+  //   }
+  // }, [data])
+  // if(!loading){setAchievements(timelineArray(achievements))}
 
-
-    return (
-      <Stack gap={3}>
-        {/* <div style={{ width: "500px", height: "400px" }}>
-          <Chrono items={timelineArray(achievements)} mode="VERTICAL" />
-        </div>
-        <div>
-          <Form.Label htmlFor="inputPost">Post your Achievement</Form.Label>
-          <Form.Control
-            type="Input"
-            id="inputPost"
-            defaultValue="Achievement Title..."
-          />
-          <Form.Control
-            type="Input"
-            id="inputPost"
-            defaultValue="Achievement Description..."
-          />
-          <Form.Text
-            id="newPost"
-            muted
-            placeholder=" Post your Achievement Here"
-          ></Form.Text>
-        </div> */}
-      </Stack>
-    );
+  return (
+    <Stack gap={3}>
+      <div style={{ width: "500px", height: "400px" }}>
+        {achievementList ? <Chrono items={items} mode="VERTICAL" /> : ""}
+      </div>
+      <div>
+        <Form.Label htmlFor="inputPost">Post your Achievement</Form.Label>
+        <Form.Control
+          type="Input"
+          id="inputPost"
+          defaultValue="Achievement Title..."
+        />
+        <Form.Control
+          type="Input"
+          id="inputPost"
+          defaultValue="Achievement Description..."
+        />
+        <Form.Text
+          id="newPost"
+          muted
+          placeholder=" Post your Achievement Here"
+        ></Form.Text>
+      </div>
+    </Stack>
+  );
 }
