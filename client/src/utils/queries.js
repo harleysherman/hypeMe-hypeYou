cba970e0d2cea6ea1554f2872d42515ccd8b2c19
harleysherman <harsatr11@icloud.com>
2024-03-11 10:27:25 -0500

    added queries and mutations for serverside

---
+++ client/src/utils/queries.js
@@ -0,0 +1,60 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_ACHIEVEMENTS = gql`
+  query getAchievements {
+    achievement {
+      _id
+      titleAchievement
+      body
+      comments {
+        commentBody
+        username
+      }
+    }
+  }
+`;
+
+export const QUERY_SINGLE_ACHIEVEMENT = gql`
+  query getSingleThought($achievementId: ID!) {
+    achievement(achievementId: $achievementId) {
+      _id
+      titleAchievement
+      body
+      comments {
+        commentBody
+        username
+      }
+    }
+  }
+`;
+
+export const QUERY_PROFILE = gql`
+  query profile {
+    profile {
+      _id
+      username
+      email
+      titleAchievement
+      body
+      comments {
+        commentBody
+        username
+      }
+    }
+  }
+`;
