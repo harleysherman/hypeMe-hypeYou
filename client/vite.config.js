d224f7bd1efb0769f1dfca62b05e2109f83be8cd
ThaDarkLord <daniel.j.sykes92@gmail.com>
2024-03-12 19:08:11 -0400

    removed vitepwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [react()],
   server: {
     port: 3000,
     open: true,
